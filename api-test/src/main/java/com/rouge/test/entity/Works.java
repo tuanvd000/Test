c055fb917ef262b7b3b1517f198f2349eb9fd2e8
tuan.vodinh <tuanvd000@gmail.com>
2019-09-16 13:10:39 +0700

    Change auto generation type from AUTO to IDENTIFY

--- api-test/src/main/java/com/rouge/test/entity/Works.java
+++ api-test/src/main/java/com/rouge/test/entity/Works.java
@@ -17,7 +17,7 @@ public class Works implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name= "id")
 	int id;
 	
